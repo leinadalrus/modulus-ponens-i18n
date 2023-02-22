11b534df837cbdf66dfde37dc7339ad9581d31c1
David <daniel.david.surla@gmail.com>
2023-02-22 01:05:03 +0000

    updated modulus ponens' main.rs and dependencies' binaries/libs

--- modulus-ponens/src/main.rs
+++ modulus-ponens/src/main.rs
@@ -15,7 +15,7 @@ fn main() {
     let built_cookie_client = reqwest::blocking::Client::builder().cookie_store(true).build()?;
     client.get(url_to_be_examined).send()?;
 
-    let html_raw_document = request_throttled();
+    let html_raw_document = request_throttled(url_to_be_examined);
     let document_selector = Document::from(&html_raw_document);
 
     // atomic lookaround regex
@@ -23,23 +23,26 @@ fn main() {
     let video_iframe_element = Regex::new(r"/^(?<(<)\b($iframe)\b$/+?>(>))").unwrap();
 
     for iframe_tag_element in document_selector
-        .find(Name(video_iframe_element.as_str().to_owned()).descendant(Attr("id", "arc")))
+        .find(Name(video_iframe_element.as_str().to_owned()).descendant(Attr("id", "arc"))) // change to "src" and value
     {
         let iframe_tag_refstr_convert = &iframe_tag_element.to_str();
-        let tag_refstr_selection_comparator = Selector::parse(iframe_tag_refstr_convert);
+        let tag_refstr_selection_comparator = &Selector::parse(iframe_tag_refstr_convert).expect("Element error | undefined | unknown");
         let element_tag_iframe = iframe_tag_element
             .select(tag_refstr_selection_comparator)
-            .next()
-            .expect("Element undefined | unknown");
+            .flat_map(|element| element.text())
+            .collect();
 
         // have the fragment be the selector's selected HTML nodes
-        let fragment = Html::parse_fragment(tag_refstr_selection_comparator);
+        let fragment = Html::parse_fragment(element_tag_iframe);
         let children_fragments = fragment
-            .select(tag_refstr_selection_comparator)
+            .find(Attr("allowfullscreen", "allowfullscreen")) // find the lang-setup exported function
+            .next()
+            .unwrap() // unwrap to get the raw-string values
+            .find(Attr("referrerpolicy", "origin"))
             .next()
             .unwrap();
         let descendant_body = children_fragments.text().collect::<Vec<&str>>();
-        let regex_matcher = class_template_element.find(descendant_body);
+        let regex_matcher = video_iframe_element.find(descendant_body);
 
         assert_eq!(
             vec![
