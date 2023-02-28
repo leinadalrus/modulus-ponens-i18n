2a6feaf178f0a2ed070b2f57faebcc0b607e5717
David <daniel.david.surla@gmail.com>
2023-02-28 03:09:32 +0000

    updated target dependencies

--- src/data/pg_client_interface.rs
+++ src/data/pg_client_interface.rs
@@ -1,20 +1,31 @@
-use sqlx::{
-    Connection,
-    postgres::PgPoolOptions
-};
+use sqlx::{postgres::PgPoolOptions, Connection};
 use std::result::Result;
 
+#[derive(sqlx::FromRow)]
+struct User {
+    id: i64,
+    email: String,
+    username: String,
+}
+
 async fn pg_pool_init() -> Result<(), sqlx::Error> {
+    let postgres_password = "Crudux:Cruo_i18n";
     let pool = PgPoolOptions::new()
         .max_connections(1)
-        .connect("postgres://postgres:password@localhost/captions").await?;
+        .connect("postgres://postgres:" + postgres_password + "@localhost/captions")
+        .await?;
 
     // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
-    let row: (u8,) = sqlx::query_as("SELECT $1")
-        .bind(1_u8)
-        .fetch_one(&pool).await?;
+    let mut rows = sqlx::query_as::<_, User>("SELECT * FROM users WHERE email = ? OR username = ?")
+        .bind(email)
+        .bind(username)
+        .fetch_one(&pool)
+        .await?;
 
-    assert_eq!(row.0, 1);
+    while let Some(rows) = rows.try_next().await? {
+        let email: &str = rows.try_get("email")?;
+        let username: &str = rows.try_get("username")?;
+    }
 
     Ok(())
 }
---
+++ target/debug/deps/socket2-839fa589f6d69385.d
@@ -0,0 +1,9 @@
+F:\Sources\Repos\modulus-ponens-i18n\target\debug\deps\socket2-839fa589f6d69385.rmeta: C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\lib.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sockaddr.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\socket.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sockref.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sys\windows.rs
+
+F:\Sources\Repos\modulus-ponens-i18n\target\debug\deps\socket2-839fa589f6d69385.d: C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\lib.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sockaddr.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\socket.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sockref.rs C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sys\windows.rs
+
+C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\lib.rs:
+C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sockaddr.rs:
+C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\socket.rs:
+C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sockref.rs:
+C:\Users\danie\.cargo\registry\src\github.com-1ecc6299db9ec823\socket2-0.4.7\src\sys\windows.rs:
