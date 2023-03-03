b19ce499410f012634227cb3b3f65c123dceb861
David <daniel.david.surla@gmail.com>
2023-03-03 03:18:27 +0000

    updated modulus-ponens/src/main.rs

--- modulus-ponens/src/main.rs
+++ modulus-ponens/src/main.rs
@@ -7,7 +7,28 @@ use select::{
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
-pub fn slow_interf_reqs(url: &str) -> std::result::Result<String, Box<dyn std::error::Error>> {
+fn cli_interf_args(_argc: i8, _argv: u8) -> ! {
+    if _argc >= 1 {
+        match _argv.offset(1) as char {
+            '1' =>
+                assert!(std::process::Command::new("npm i; npm build; npm run")
+                    .spawn()
+                    .is_ok()),
+            '2' => assert!(std::process::Command::new(
+                "cd $(ls | grep -r 'entrypoint.sh' ./); ./entrypoint.sh"
+            )
+            .spawn()
+            .is_ok()),
+            _ => panic!(),
+        }
+    }
+
+    core::intrinsics::abort();
+}
+
+pub fn slow_interf_reqs(
+    url: &str,
+) -> std::result::Result<String, Box<dyn std::error::Error>> {
     url = match std::env::args().nth(1) {
         Some(url) => &url,
         None => {
@@ -15,7 +36,8 @@ pub fn slow_interf_reqs(url: &str) -> std::result::Result<String, Box<dyn std::e
             "https://allanime.to/watch/YeWtc8REZAGKPeb6q/kage-no-jitsuryokusha-ni-naritakute-/episode-1-sub".into()
         }
     };
-    let response = reqwest::blocking::get(url).expect("URL undefined | unknown");
+    let response =
+        reqwest::blocking::get(url).expect("URL undefined | unknown");
     Ok(response.text().unwrap())
 }
 
@@ -29,11 +51,11 @@ pub fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    let body = reqwest::get(url_to_be_examined)
-    .text()
-    .await?;
+    let body = reqwest::get(url_to_be_examined).text().await?;
 
-    let built_cookie_client = reqwest::blocking::Client::builder().cookie_store(true).build()?;
+    let built_cookie_client = reqwest::blocking::Client::builder()
+        .cookie_store(true)
+        .build()?;
     client.get(url_to_be_examined).send()?;
 
     let html_raw_document = request_throttled(url_to_be_examined);
@@ -41,13 +63,19 @@ pub fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
 
     // atomic lookaround regex
     let class_template_element = Regex::new(r"/^(?=(<\W+/>))$/").unwrap();
-    let video_iframe_element = Regex::new(r"/^(?<(<)\b($iframe)\b$/+?>(>))").unwrap();
+    let video_iframe_element =
+        Regex::new(r"/^(?<(<)\b($iframe)\b$/+?>(>))").unwrap();
 
-    for iframe_tag_element in document_selector
-        .find(Name(video_iframe_element.as_str().to_owned()).descendant(Attr("id", "arc"))) // change to "src" and value
+    for iframe_tag_element in document_selector.find(
+        Name(video_iframe_element.as_str().to_owned())
+            .descendant(Attr("id", "arc")),
+    )
+    // change to "src" and value
     {
         let iframe_tag_refstr_convert = &iframe_tag_element.to_str();
-        let tag_refstr_selection_comparator = &Selector::parse(iframe_tag_refstr_convert).expect("Element error | undefined | unknown");
+        let tag_refstr_selection_comparator =
+            &Selector::parse(iframe_tag_refstr_convert)
+                .expect("Element error | undefined | unknown");
         let element_tag_iframe = iframe_tag_element
             .select(tag_refstr_selection_comparator)
             .flat_map(|element| element.text())
@@ -71,7 +99,8 @@ pub fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                 regex_matcher.expect("\0").end()
             ],
             vec![descendant_body.len()]
-        ); // NOTE(David): non-throttled `loop {}` condition would be nice for testing
+        ); // NOTE(David): non-throttled `loop {}` condition would be nice for
+           // testing
     }
 
     Ok(())
