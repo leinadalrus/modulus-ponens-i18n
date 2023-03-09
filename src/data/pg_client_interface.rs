d03922498f038c1aebba638b9cf2fe7614281b4a
David <daniel.david.surla@gmail.com>
2023-03-09 21:32:32 +0000

    Update pg_client_interface.rs

--- src/data/pg_client_interface.rs
+++ src/data/pg_client_interface.rs
@@ -34,27 +34,21 @@ struct Video {
     captions: Caption,
 }
 
-async fn pg_pool_init() -> Result<(), external_sqlx::Error> {
+async fn pg_pool_init(user: User) -> Result<(), external_sqlx::Error> {
     let postgres_password = "Crudux:Cruo_i18n";
-    let pool =
-        PgPoolOptions::new().max_connections(1).connect(
-            &"postgres://postgres:ehm4rn3ndx_wky3xep@localhost/captions"
-                .to_owned(), // Fake password
-        ); // TODO(Config): save this connection data into a safe JSON/Configfile.
+    let postgres_url =
+        &"postgres://postgres:ehm4rn3ndx_wky3xep@localhost/captions".to_owned();
+    let pool = PgPoolOptions::new().max_connections(1).connect(
+        &postgres_url, // Fake password
+    ); // TODO(Config): save this connection data into a safe JSON/Configfile.
 
     // Make a simple query to return the given parameter (use a question mark
     // `?` instead of `$1` for MySQL)
     let mut rows = external_sqlx::query_as::<_, User>(
         "SELECT * FROM users WHERE email = ? OR username = ?",
     )
-    .bind(email)
-    .bind(username);
-
-    let mut videos = external_sqlx::query_as::<_, Video>(
-        "SELECT * FROM videos WHERE id = ? or id = 0",
-    )
-    .bind(username)
-    .bind(title);
+    .bind(user.email)
+    .bind(user.username);
 
     while let Ok(Some(rows)) = rows.try_into() {
         let email: &str = "email";
@@ -65,11 +59,11 @@ async fn pg_pool_init() -> Result<(), external_sqlx::Error> {
 }
 
 async fn pg_pool_insert(
-    insertion_args: &str,
+    videos: Video, insertion_args: &str,
 ) -> Result<(), external_sqlx::Error> {
     let mut rows = external_sqlx::query_as::<_, Video>(insertion_args)
-        .bind(user)
-        .bind(title);
+        .bind(videos.user.username)
+        .bind(videos.title);
 
     while let Ok(Some(rows)) = rows.try_into() {
         let user: &str = "user";
@@ -79,10 +73,12 @@ async fn pg_pool_insert(
     Ok(())
 }
 
-async fn pg_pool_update(update_args: &str) -> Result<(), external_sqlx::Error> {
+async fn pg_pool_update(
+    videos: Video, update_args: &str,
+) -> Result<(), external_sqlx::Error> {
     let mut rows = external_sqlx::query_as::<_, Video>(update_args)
-        .bind(user)
-        .bind(title);
+        .bind(videos.user.username)
+        .bind(videos.title);
 
     while let Ok(Some(rows)) = rows.try_into() {
         let user: &str = "user";
@@ -93,11 +89,11 @@ async fn pg_pool_update(update_args: &str) -> Result<(), external_sqlx::Error> {
 }
 
 async fn pg_pool_destroy(
-    deletion_args: &str,
+    videos: Video, deletion_args: &str,
 ) -> Result<(), external_sqlx::Error> {
     let mut rows = external_sqlx::query_as::<_, Video>(deletion_args)
-        .bind(user)
-        .bind(title);
+        .bind(videos.user.username)
+        .bind(videos.title);
 
     while let Ok(Some(rows)) = rows.try_into() {
         let user: &str = "user";
